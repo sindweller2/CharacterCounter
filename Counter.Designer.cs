6eb42717ea041b734e06ddc6b3226e58425e5ed9
sindweller2 <eternal_dante@yahoo.com>
2018-01-04 02:58:05 +0700

    Add files via upload

---
+++ Counter.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace CharacterCounter
+{
+    partial class Counter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.textBoxCharacterCounter = new System.Windows.Forms.TextBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.Color.White;
+            this.richTextBox.Location = new System.Drawing.Point(15, 18);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox.Size = new System.Drawing.Size(356, 88);
+            this.richTextBox.TabIndex = 0;
+            this.richTextBox.Text = "";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Location = new System.Drawing.Point(12, 129);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(59, 13);
+            this.label.TabIndex = 1;
+            this.label.Text = "Character :";
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Location = new System.Drawing.Point(293, 112);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 34);
+            this.buttonProcess.TabIndex = 3;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
+            // 
+            // textBoxCharacterCounter
+            // 
+            this.textBoxCharacterCounter.BackColor = System.Drawing.Color.White;
+            this.textBoxCharacterCounter.Location = new System.Drawing.Point(77, 126);
+            this.textBoxCharacterCounter.Name = "textBoxCharacterCounter";
+            this.textBoxCharacterCounter.ReadOnly = true;
+            this.textBoxCharacterCounter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCharacterCounter.TabIndex = 4;
+            this.textBoxCharacterCounter.WordWrap = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(212, 112);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 34);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Counter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(380, 158);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.textBoxCharacterCounter);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.richTextBox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Counter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Character Counter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.TextBox textBoxCharacterCounter;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Counter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CharacterCounter
+{
+    public partial class Counter : Form
+    {
+        public Counter()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonProcess_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.textBoxCharacterCounter.Text = this.richTextBox.Text.Length.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.richTextBox.Clear();
+                this.textBoxCharacterCounter.Clear();
+                this.richTextBox.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
